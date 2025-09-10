3fd6aeb36902291ea9eb28002f1482b674630067
Avadhut Thorat <avadhutthorat40@gmail.com>
2025-09-10 11:49:05 +0530

    Added otp storybook

---
+++ ReactJs-Typescript/src/stories/Otp.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { createElement } from "react";
+import Otp from "@components/Otp/index.tsx";
+
+const meta = {
+  title: " Example/Otp",
+  component: Otp,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  args: {
+    length: 5,
+  },
+} satisfies Meta<typeof Otp>;
+
+export default meta;
+
+type Story = StoryObj<typeof Otp>;
+
+export const Default: Story = {
+  args: {
+    length: 5,
+  },
+};
